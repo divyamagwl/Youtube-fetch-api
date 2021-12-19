20d194569ec34c9ab509cc0113ae548e6a0d4933
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2021-12-19 23:58:18 +0530

    Added Videos List API View

--- api/youtube_api/views.py
+++ api/youtube_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, filters
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+from .models import Video
+from .serializers import VideoSerializer, YoutubeAPIKeySerializer
+
+class VideosListAPI(generics.ListAPIView):
+    """
+    API for getting list of all the videos, order by latest published time.
+    """
+    queryset = Video.objects.all()
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+   
+    # Adding the search and filter fields
+    search_fields = ['title']
+    filter_fields = ['channelTitle']
+
+    ordering = ['-publishedAt'] # Sorting the videos in reverse chronological of publish date time
+
+    serializer_class = VideoSerializer
