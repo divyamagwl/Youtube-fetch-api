40ee5487c577d846aa02eda10b364769ce16cd75
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2021-12-19 23:55:04 +0530

    Added Video and Youtube API key models

--- api/youtube_api/models.py
+++ api/youtube_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=255) # Title of the video
+    description = models.CharField(max_length=1000) # Description of the video
+    publishedAt = models.DateTimeField() # Publish date time Of the video
+    thumbnails = models.URLField() # URL Of the video thumbnails
+    channelId = models.CharField(max_length=100) # Id Of the channel
+
+    def __str__(self):
+        return self.title
+
+class YoutubeAPIKey(models.Model):
+    key = models.TextField()
+    quotaFinished = models.BooleanField(default=False)
