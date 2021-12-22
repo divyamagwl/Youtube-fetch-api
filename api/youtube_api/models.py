0d3d7611c826aa48ce5ade6af223f4341551e0e0
Divyam Agrawal <agrawaldivyam2001@gmail.com>
2021-12-23 01:25:29 +0530

    Updated Video model attributes

--- api/youtube_api/models.py
+++ api/youtube_api/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Video(models.Model):
+    videoId = models.CharField(max_length=100, unique=True) # Unique id of the video
     title = models.CharField(max_length=255)  # Title of the video
     description = models.CharField(max_length=1000)  # Description of the video
     publishedAt = models.DateTimeField()  # Publish date time Of the video
-    thumbnails = models.URLField()  # URL Of the video thumbnails
+    thumbnail = models.URLField()  # URL Of the video thumbnails
     channelId = models.CharField(max_length=100)  # Id Of the channel
+    channelTitle = models.CharField(max_length=200) # Name of the channel
 
     def __str__(self):
         return self.title
